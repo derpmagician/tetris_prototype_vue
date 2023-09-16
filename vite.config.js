c5ea60f535f4879fb2d8d637c384e274803160a4
derpmagician <renzin@gmail.com>
2023-09-15 19:50:09 -0500

    adding base name

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,9 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const ASSET_URL = process.env.ASSET_URL || '';
-
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: `${ASSET_URL}/dist/`,
-  publicPath: process.env.NODE_ENV === 'production' ? '' : '/',
+  base: `/tetris_prototype_vue/`,
   plugins: [
     vue(),
   ],
